afb69323d33cd608bbcd595e8260cea2c9f7d0ae
janbarmo <mohammed.janbar-ext@natixis.com>
2024-08-08 15:32:53 +0200

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello world!");
+        CalculateHandShake c = new CalculateHandShake();
+    }
+}
